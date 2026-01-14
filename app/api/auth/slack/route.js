935e427b1ea28ee91d32ada759c992d1ba5e35aa
alexblackrabbit <alex@blackrabbitgroup.co>
2026-01-13 16:28:25 -0800

    Update route.js

--- app/api/auth/slack/route.js
+++ app/api/auth/slack/route.js
@@ -3,19 +3,12 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const params = new URLSearchParams({
     client_id: process.env.SLACK_CLIENT_ID,
-    scope: [
-      "openid",
-      "profile",
-      "email",
-      "users:read.email",
-      "team:read",
-      "users:read"
-    ].join(" "),
+    scope: ["openid", "profile", "email"].join(" "),
     redirect_uri: process.env.SLACK_REDIRECT_URI,
     response_type: "code"
   });
 
-  const slackAuthUrl = `https://slack.com/openid/connect/authorize?${params.toString()}`;
-
-  return NextResponse.redirect(slackAuthUrl);
+  return NextResponse.redirect(
+    `https://slack.com/openid/connect/authorize?${params.toString()}`
+  );
 }
